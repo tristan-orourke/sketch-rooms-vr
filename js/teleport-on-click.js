7c4c7fd8ae30a11994e667f1b8422732a21f63b2
tristan-orourke <tristan.m.orourke@gmail.com>
2021-12-04 02:27:11 -0500

    Some transition between rooms, but fading animation not working

---
+++ js/teleport-on-click.js
@@ -0,0 +1,40 @@
+AFRAME.registerComponent('teleport-on-click', {
+  schema: {
+    from: { type: 'selector' },
+    to: { type: 'selector' }
+  },
+  init: function () {
+    document.querySelector('#curtain').addEventListener('teleport', function logTeleport(event) {
+      console.log('teleport registered');
+      // document.querySelector('#curtain').material.opacity =0.5;
+      
+    });
+  },
+  events: {
+    click: function () {
+      const from = this.data.from;
+      const to = this.data.to;
+
+      // Deactivate clickable elements in the object disappearing.
+      const oldClickableElements = from.querySelectorAll('.clickable');
+      for (var i = 0; i < oldClickableElements.length; i++) {
+        oldClickableElements[i].classList.remove("active");
+      }
+
+      // Activate clickable elements in the object appearing
+      const newClickableElements = to.querySelectorAll('.clickable');
+      for (var i = 0; i < newClickableElements.length; i++) {
+        newClickableElements[i].classList.add("active");
+      }
+
+      // Start the fade-out
+      document.querySelector("#curtain").emit("teleport", this.data, true);
+      
+      // Replace objects when curtain is up
+      setTimeout(function switchObjs() {
+        from.object3D.visible = false;
+        to.object3D.visible = true;
+      }, 500);
+    }
+  }
+});
