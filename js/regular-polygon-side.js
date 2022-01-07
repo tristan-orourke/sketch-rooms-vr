f929f09dd796cc1c73d87ee4129cefc018493a88
tristan-orourke <tristan.m.orourke@gmail.com>
2022-01-06 20:36:53 -0500

    Fix inversion of rooms

--- js/regular-polygon-side.js
+++ js/regular-polygon-side.js
@@ -14,8 +14,8 @@ AFRAME.registerComponent('regular-polygon-side', {
       data.sides !== oldData?.sides || 
       data.index !== oldData?.index) {
       const apothem = this.calculateApothen(data.sides, data.width);
-      const rotation = this.calculateRotation(data.index, data.sides);
-      const position = this.calculatePosition(data.index, data.sides, apothem);
+      const rotation = this.calculateRotation(data.sides - data.index, data.sides); // Use sides - index to go around clockwise instead of counter-clockwise.
+      const position = this.calculatePosition(data.sides - data.index, data.sides, apothem);
       el.object3D.rotation.set(...rotation);
       el.object3D.position.set(...position);
     }
@@ -26,12 +26,15 @@ AFRAME.registerComponent('regular-polygon-side', {
   calculateOuterRadius: function (numberOfSides, lengthOfSide) {
     return lengthOfSide / (2 * Math.sin(Math.PI / numberOfSides));
   },
+  calculateAngle: function (i, numberOfSides) {
+    return (i * 2 * Math.PI / numberOfSides);
+  },
   calculatePosition: function (i, numberOfSides, apothem) {
-    const angle = i * 2 * Math.PI / numberOfSides;
+    const angle = this.calculateAngle(i, numberOfSides);
     return [Math.sin(angle)*apothem, 0, Math.cos(angle)*apothem];
   },
   calculateRotation: function (i, numberOfSides) {
-    const angle = i * 2 * Math.PI / numberOfSides;
+    const angle = this.calculateAngle(i, numberOfSides) + Math.PI;
     return [0, angle, 0];
   }
 });
